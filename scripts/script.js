d7ac75a7d66c0f255d671060d990f5561e22010a
nnicusor <nicu.scurtu10@gmail.com>
2019-09-01 18:50:46 +0300

    Remainder to do not array function on services

--- scripts/script.js
+++ scripts/script.js
@@ -1,55 +1,64 @@
 
-angular.module('myModule', ['ngCookies']).controller("myController", ['$scope', '$http', '$cookies', 'handleCookies',
-    ($scope, $http, $cookies, handleCookies) => {
-    $http.get('https://api.github.com/users')
-        .then((data) => {
-        $scope.users_data_array = {
-            users_data: data.data,
-            usersLength: data.data.length
-        }
-    });
-    $http.get('https://api.github.com/repositories')
-        .then((response) => {
-        $scope.repos_data_array = {
-            repos_data: response.data,
-            reposLength: response.data.length
-        }
-    });
+angular.module('myModule', ['ngCookies']).controller("myController", ['$scope', '$http', '$cookies', 'handleCookies', 'handleArrays',
+    ($scope, $http, $cookies, handleCookies, handleArrays) => {
+        $http.get('https://api.github.com/users')
+            .then((data) => {
+                $scope.users_data_array = {
+                    users_data: data.data,
+                    usersLength: data.data.length
+                }
+            });
+        // let changed_repos = [];
+        let changed_repos = handleCookies.getCookieValue($cookies, 'changed_repos_cookie');
 
-    $scope.description = '';
+        $http.get('https://api.github.com/repositories')
+            .then((response) => {
+                $scope.repos_data_array = {
+                    repos_data: response.data,
+                    reposLength: response.data.length
+                };
 
-    const changed_repos = handleCookies.getCookieValue($cookies, 'changed_repos_cookie');
+                if(changed_repos) {
+                    for(let i = 0; i < changed_repos.length; ++i) {
+                        const index = handleArrays.binarySearch($scope.repos_data_array.repos_data, changed_repos[1].ID);
+                        if(index) $scope.repos_data_array.repos_data[1].description = changed_repos[1].description;
 
-    $scope.saveDescription = (repo_description, repo) => {
-        console.log(1, repo_description, repo);
+                    }
+                }
+            });
 
-        let found = false;
-        for(let i = 0; i < changed_repos.length; ++i) {
-            if (repo.id === changed_repos[i].ID) {
-                changed_repos[i].description = repo_description;
-                found = true;
-                break;
+        $scope.description = '';
+
+        $scope.saveDescription = (repo_description, repo) => {
+            console.log(1, repo_description, repo);
+
+            let found = false;
+            for (let i = 0; i < changed_repos.length; ++i) {
+                if (repo.id === changed_repos[i].ID) {
+                    changed_repos[i].description = repo_description;
+                    found = true;
+                    break;
+                }
             }
-        }
 
-        if(!found){
-            console.log('Object to push:', {
-                ID: repo.id,
-                description: repo_description,
-            });
+            if (!found) {
+                console.log('Object to push:', {
+                    ID: repo.id,
+                    description: repo_description,
+                });
 
-            changed_repos.push({
-                ID: repo.id,
-                description: repo_description,
-            });
-        }
+                changed_repos.push({
+                    ID: repo.id,
+                    description: repo_description,
+                });
+            }
 
-        console.log(3, changed_repos);
-        handleCookies.addCookie($cookies, changed_repos);
-    };
+            console.log(3, changed_repos);
+            handleCookies.addCookie($cookies, changed_repos);
+        };
 
-    $scope.rowLimit = 25;
-}]).service('handleCookies', function() {
+        $scope.rowLimit = 25;
+}]).service('handleCookies', function () {
     this.addCookie = (cookieScope, cookie_objects) => {
         // console.log(4, cookie_objects);
         cookieScope.remove('changed_repos_cookie');
@@ -72,11 +81,27 @@ angular.module('myModule', ['ngCookies']).controller("myController", ['$scope', 
     };
 
     this.getCookieValue = (cookieScope, key) => {
-        return cookieScope.getObject(key);
+        const cookie = cookieScope.getObject(key);
+        return cookie ? cookie : [];
     }
+ }).service('handleArrays',function () {
+    this.sortArrayAsc = (array) => {
+        return array.sort();
+    };
+
+    this.binarySearch = (array, key) => {
+        let left = 0;
+        let right = array.length - 1;
+
+        let mid;
+        while(left <= right) {
+            mid = ( left + right ) / 2;
+
+            if(array[mid] === key) return mid;
+
+            if(array[mid] < key) left = mid + 1;
+            else right = mid - 1;
+        }
+        return -1;
+    };
 });
-
-
-
-
-
